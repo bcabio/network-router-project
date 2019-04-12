effe1cec50662c4833eb9b5cfa1429edce2f25b9
Kevin Aiken <Aiken.Kevin.M@gmail.com>
2019-04-12 17:23:36 -0400

    message body changed to json with dest and body

--- client.py
+++ client.py
@@ -1,10 +1,23 @@
 import socket
+import json
 
-UDP_IP = "192.168.1.95"
+
+# Convert IP to hex
+def ip2hex(ip):
+    ip1 = '-'.join([hex(int(x)+256)[3:] for x in ip.split('.')])
+    return ip1
+
+
+DEST_UDP_IP = "192.168.1.122"
 UDP_PORT = 5005
-MESSAGE = b'1 hello world'
 
-print("sending")
+message = {
+    "destination": socket.inet_aton(DEST_UDP_IP).hex().upper(),
+    "message": "Hello, this is a client"
+}
+
+
+print("Sending " + json.dumps(message))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(json.dumps(message).encode('raw_unicode_escape'), (DEST_UDP_IP, UDP_PORT))
