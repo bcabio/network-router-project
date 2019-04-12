bb0df0be6478c79b2502697d827fc3d7fffdb651
Kevin Aiken <Aiken.Kevin.M@gmail.com>
2019-04-12 16:09:38 -0400

    initial commit

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+
+UDP_IP = "192.168.0.95"
+UDP_PORT = 5005
+MESSAGE = b'1 hello world'
+
+print("sending")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
